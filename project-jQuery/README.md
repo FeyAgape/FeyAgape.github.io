cb6ff5c23ecc73798d78e67994958101644ecbc8
Fey Ijaware <Fey.ijaware@talent-works.com>
2018-05-10 16:19:01 +0100

    added a async project using the jquery ajax method

---
+++ project-jQuery/README.md
@@ -0,0 +1,9 @@
+[Website](https://feyagape.github.io/project-jQuery/)
+
+ Using Asynchronous data request. Using the The `.ajax()` method to send and recieve data Asynchronous. The site uses the Unsplash API for images and the NYT API for the articles.
+
+### jQuery's `.ajax()` Usage 
+
+Using jQuery's `.ajax()` method, there's less setup code that you need to manage. That's good, but to use jQuery we also have to include the entire library and force our users to download the entire thing every time.
+
+
---
+++ project-jQuery/js/app.js
@@ -0,0 +1,50 @@
+(function () {
+    const form = document.querySelector('#search-form');
+    const searchField = document.querySelector('#search-keyword');
+    let searchedForText;
+    const responseContainer = document.querySelector('#response-container');
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        responseContainer.innerHTML = '';
+        searchedForText = searchField.value;
+
+
+        $.ajax({
+        	url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+        	headers: {
+        		Authorization:`Client-ID faa6734d78701fc28e60f034c34fe2e3ec655c16e50dfc8a0b77c2bd3eb34f6e`
+        	}
+        }).done(addImage);
+
+         $.ajax({
+        	url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=f1c69a7b01c34968b7e4da055884e63b`
+        }).done(addArticles);
+
+    });
+
+    function addImage(images) {
+    const firstImage = images.results[0];
+
+    responseContainer.insertAdjacentHTML('afterbegin', `<figure>
+            <img src="${firstImage.urls.regular}" alt="${searchedForText}">
+            <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
+        </figure>`
+    );
+}
+
+    function addArticles(data) {
+
+    const articlesInHTML = data.response.docs.map(article => `<li>
+                <h2>${article.headline.main}<h2>
+                <p>${article.snippet}</p>
+                <p>Word Count: ${article.word_count}<br> Type of Material: ${article.type_of_material}<br>Source: ${article.source}</p>
+                <p><button class="button"><a href="${article.web_url}">View Article</a></button></p>
+            </li>`
+        );
+
+        responseContainer.insertAdjacentHTML('beforeend', `<ul>${articlesInHTML.join('')}</ul>`);
+    }
+})();
+
+
